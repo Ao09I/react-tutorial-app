a30fe2bcc45ac0f533fe442ee84da4df4e0bfe00
seri <111559923+Ao09I@users.noreply.github.com>
2023-05-25 09:53:09 +0000

    reactバージョン変更

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+import { StrictMode } from "react";
+import ReactDom from "react-dom";
+
+import App from "./App";
+
+ReactDom.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
